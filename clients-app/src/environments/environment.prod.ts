df3e4dca0548fc30afd64f30eb0667411d49bfa1
LayzaDev <layzanauanedev@gmail.com>
2025-03-04 09:21:16 -0300

    Fix: Bug fix in the environment

--- clients-app/src/environments/environment.prod.ts
+++ clients-app/src/environments/environment.prod.ts
@@ -1,4 +1,1 @@
-export const environment = {
-  production: true,
-  apiUrl: process.env.API_URL_PROD
-};
+export const environment = {};
--- clients-app/src/environments/environment.ts
+++ clients-app/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: process.env.API_URL_DEV
+  API_URL_DEV: '',
+  API_URL_PROD: ''
 };
 
 /*
